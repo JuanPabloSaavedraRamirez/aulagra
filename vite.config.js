28c25c32619a2aabf33192e44a15b03439496616
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2023-11-25 01:12:07 -0600

    solucion 23

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['CuentoCorto.JPG']
-  },
+    optimizeDeps: {
+    exclude: ['/public/CuentoCorto.JPG']
+  }
   plugins: [
     vue(),
     vueJsx(),
