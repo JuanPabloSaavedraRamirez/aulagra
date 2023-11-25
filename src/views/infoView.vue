1658ebc9d24ce27e330d31c5520c820e6d463322
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2023-11-25 01:57:27 -0600

    solucion 44

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -33,8 +33,8 @@
     :autoplay="{ delay: 1000 }"
     class="mySwiper">
 
-  <swiper-slide><img src="/public/1.JPG" alt=""></swiper-slide>
-  <swiper-slide><img src="/public/2.JPG" alt=""></swiper-slide>
+  <!-- <swiper-slide><img src="/public/1.JPG" alt=""></swiper-slide>
+  <swiper-slide><img src="/public/2.JPG" alt=""></swiper-slide> -->
   <!-- <swiper-slide><img src="/public/3.JPG" alt=""></swiper-slide>
   <swiper-slide><img src="/public/4.JPG" alt=""></swiper-slide>
   <swiper-slide><img src="/public/5.JPG" alt=""></swiper-slide> -->
