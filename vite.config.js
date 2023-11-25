3cc6082606c5f6d4f97c416d5b5d42030dc22629
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2023-11-25 01:42:59 -0600

    solucion 31

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -10,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
